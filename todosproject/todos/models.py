3a3f82e38f77c26000256dba3be77c9b70a979b8
Igor Kyrf <kyrf.pda@gmail.com>
2022-03-14 21:28:03 +0500

    lesson-001 new branch

--- todosproject/todos/models.py
+++ todosproject/todos/models.py
@@ -1,7 +1,6 @@
 from uuid import uuid4
 from django.db import models
 
-
 class Users(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid4)
     first_name = models.CharField(max_length=64)
